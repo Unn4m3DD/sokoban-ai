e10865e88eb2cc4c41c5b2bf65acef0b50f39037
André Patacas <guerreiroapatacas@gmail.com>
2020-10-10 17:18:56 +0100

    agent started

--- agent.py
+++ agent.py
@@ -6,6 +6,7 @@ class Agent:
   def __init__(self, map, timeout=math.inf):
     self.current = -1
     self.plays = ["s", "a", "w", "d", "d", "d", "s", "a"]
+    print(map.empty_goals)
 
   async def query_move(self, websocket):
     self.current += 1
@@ -13,3 +14,5 @@ class Agent:
       await websocket.send(
           json.dumps({"cmd": "key", "key": self.plays[self.current]})
       )
+
+partes(lista[1:]) + map partes(lista[1:]), item => item.appeng(lista[1])
