a6e35336b6c2fa436e77c1bcab2cd1e1f0307068
Andre Patacas <guerreiroapatacas@gmail.com>
2020-11-21 17:35:10 +0000

    thread implementation

--- src/client.py
+++ src/client.py
@@ -6,11 +6,20 @@ from time import time
 import websockets
 from src.agent import Agent
 from time import sleep
+import threading
+
+
+def set_interval(func, sec):
+  def func_wrapper():
+    set_interval(func, sec)
+    func()
+  t = threading.Timer(sec, func_wrapper)
+  t.start()
+  return t
 
 
 async def agent_loop(server_address="localhost:8000", agent_name="student"):
   async with websockets.connect(f"ws://{server_address}/player", close_timeout=10000) as websocket:
-
     await websocket.send(json.dumps({"cmd": "join", "name": agent_name}))
     server_request = json.loads(await websocket.recv())
     print(server_request)
@@ -23,20 +32,19 @@ async def agent_loop(server_address="localhost:8000", agent_name="student"):
       solution = None
       outer_start_time = time()
       try:
+        thread = set_interval(websocket.recv, 1 / (fps + 1))
         while solution == None:
           start_time = time()
           solution = agent.solve(1, 300, fps)
           elapsed_time = time() - start_time
-          for i in range(0, int(elapsed_time * fps + 1)):
-            await websocket.recv()
-
+        thread.cancel()
         for key in solution:
           await websocket.send(
               json.dumps({"cmd": "key", "key": key})
           )
           await websocket.recv()
         print(
-            f"solved {level} in {round((time() - outer_start_time) /100) * 100} seconds")
+            f"solved {level} in {round((time() - outer_start_time) * 100) / 100} seconds")
       except websockets.exceptions.ConnectionClosedOK:
         print("Server has cleanly disconnected us")
         return
